8e1cb226598295fdf7c5682cdf730a73288339e6
fatma yamaner <82721046+fatmay@users.noreply.github.com>
2023-10-18 14:13:06 +0300

    kodluyorum

--- liveclock.js
+++ liveclock.js
@@ -1,16 +1,27 @@
 function currentDate(){
     //dom ögelerini secelim
-    let clock=document.querySelector("#myClock");
-    let today=new Date()//saati ayarlamak için
-    //günlerimiz default olarak gelmiyor ben de array oluşturdum arrayle çekeceğim
+    let clock=document.getElementById("myClock");
     let days=["pazar","pazartesi","salı","çarşamba","perşembe","cuma","cumartesi"];
-    let day=days[today.getDay()]//arrayden günleri çektim
-    let time=today.getHours()+ ":"+ today.getMinutes()+":"+ today.getSeconds()+ `${day}`
-    //saat,dakika,saniye olarak yazdırdık yanına da günlerimizi yazdırmak için arrayden çektiğim ve "day" değişkenine atadığımı template literals ile yazdım
 
-    let currentDate=`${time}`
-    clock.innerHTML=currentDate //canlı saati html ekranına yazdırıyoruz
-    setTimeout(currentDate,1000)//dinamik saniye bilgisi için
-    //setInterval(currentDate,1000)//dinamik saniye bilgisi için
+
+    function uploadClock(){
+        let today=new Date()//saati ayarlamak için
+        //günlerimiz default olarak gelmiyor ben de array oluşturdum arrayle çekeceğim
+
+        let day=days[today.getDay()]//arrayden günleri çektim
+        let time=today.getHours()+ ":"+ today.getMinutes()+":"+ today.getSeconds()+ `${day}`
+        //saat,dakika,saniye olarak yazdırdık yanına da günlerimizi yazdırmak için arrayden çektiğim ve "day" değişkenine atadığımı template literals ile yazdım
+
+        let currentDate=`${time}`
+        clock.innerHTML=currentDate //canlı saati html ekranına yazdırıyoruz
+        //setTimeout(currentDate,1000)//dinamik saniye bilgisi için
+
+    }
+    uploadClock();
+
+
+
+
 }
-currentDate();
+//currentDate();
+setInterval(currentDate,1000)//dinamik saniye bilgisi için
